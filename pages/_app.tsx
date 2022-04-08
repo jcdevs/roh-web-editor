9cacfa32d5ccd74fa05bd21d70ebf5940deac1b5
Jordan Carr <jordan@fluxcontinuum.com>
2022-04-08 13:38:31 -0500

    delint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import React from 'react';
+import type { AppProps } from 'next/app';
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
 
 export default App;
