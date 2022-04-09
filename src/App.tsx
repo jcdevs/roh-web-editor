63dade59668548bc0bd3b8db354a98950c104a93
Jordan Carr <jordan@fluxcontinuum.com>
2022-04-09 15:48:11 -0500

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import theme from './styles/theme';
 import createEmotionCache from './utils/createEmotionCache';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -16,9 +21,18 @@ const App = () => {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Dashboard>
-          <Paper sx={{display: 'flex', flexDirection: 'column', height: 600}}>
-
-          </Paper>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              {/* <Route index element={<Home />} />
+              <Route path="teams" element={<Teams />}>
+                <Route path=":teamId" element={<Team />} />
+                <Route path="new" element={<NewTeamForm />} />
+                <Route index element={<LeagueStandings />} />
+              </Route> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
         </Dashboard>
       </ThemeProvider>
     </CacheProvider>
