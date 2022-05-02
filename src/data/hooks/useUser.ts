a332fa7fc8d7bc5abd7ce248e3cc032ef05b81ca
Jordan Carr <jordan@fluxcontinuum.com>
2022-05-02 12:14:08 -0500

    remove cruft

--- src/data/hooks/useUser.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../interfaces/User";
-import { useLocalStorage } from "./useLocalStorage";
-
-export const useUser = () => {
-  const { value, setValue } = useLocalStorage<User | null>('user', null);
-
-  return { user: value, setUser: setValue };
-}
